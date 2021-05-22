ffc535b1137347db2943e541e22208ed55ae21b7
SeanDp32 <sean@igloovision.com>
2021-05-22 13:09:10 -0400

    modifiy link list

--- linkList.py
+++ linkList.py
@@ -1,30 +1,5 @@
-class Node: 
 
-	def __init__(self, value, next_node=None):
-
-		self.value = value
-		self.next_node = next_node
-
-
-	def set_next_node(self, next_node):
-		self.next_node = next_node
-
-
-	def set_value(self, value):
-		self.value = value 
-
-
-	def get_value(self):
-
-		return self.value
-
-
-	def get_next_node(self):
-		return self.next_node
-
-
-
-
+from node import Node
 
 class LinkList:
 
