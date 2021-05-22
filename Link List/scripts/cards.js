dd4a2a0a5fe13ff704175576ca3acdd25f930ec1
SeanDp32 <sean@igloovision.com>
2021-05-22 14:53:57 -0400

    modifiy link list

---
+++ Link List/scripts/cards.js
@@ -0,0 +1,15 @@
+
+import { hello } from './node.js';
+
+function setup() {
+
+	createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw(){
+
+	background(52, 152, 219);
+	//square((windowWidth/2)-300, (windowHeight/2)-340, 600);
+
+}
